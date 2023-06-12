8a96130f92b2d602a07dc2491791b41f7223eaac
Sunysian <yosra205@outlook.com>
2023-06-12 13:58:47 +0200

    loginworksbutatwhatcost

--- login.php
+++ login.php
@@ -31,7 +31,48 @@
             die("Connection failed: " . $conn->connect_error);
         }
         
-        // Close the connection
+        // Establish database connection
+        $servername = "localhost"; 
+        $username = "root";  
+        $password = "yosraDB"; 
+        $database = "mydbeks"; 
+        
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $database);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        // Retrieve user input
+        $loginusername = $_POST['loginusername'];
+        $loginpassword = $_POST['loginpassword'];
+
+        // Sanitize and validate user input (You can add more validation as per your requirements)
+        $loginusername = $conn->real_escape_string($loginusername);
+        $loginpassword = $conn->real_escape_string($loginpassword);
+
+        // Query the database
+        $query = "SELECT * FROM adminusers WHERE loginpassword = '$loginpassword'";
+        $result = $conn->query($query);
+        
+
+        if ($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $storedHashedPassword = $row['loginpassword'];
+            // Verify the password
+            // Password is correct, start a session
+            session_start();
+            $_SESSION['loginusername'] = $loginusername;
+            // Redirect to a protected area or display a success message
+            header("Location: tables.php");
+            exit();
+        } else {
+            // Invalid username or password
+            echo "Invalid username or password.";
+        }
+        
         $conn->close();
 
         
